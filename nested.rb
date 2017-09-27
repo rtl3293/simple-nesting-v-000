7ce9f4140015fbc8a473af571979c9df1c14c184
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 22:06:14 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -110,7 +110,7 @@ def changing_alan
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 
-
+		 programmer_hash
 end
 
 def adding_to_dennis
@@ -130,4 +130,5 @@ def adding_to_dennis
         }
      }
 		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 programmer_hash
 end
