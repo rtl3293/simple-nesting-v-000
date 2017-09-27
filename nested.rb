10e1e575402e40d9c62393e51d2c467ef31eb8a0
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 22:03:10 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,7 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         },
-				#:yukihiro_matsumoto => 
+				#:yukihiro_matsumoto =>
 
      }
 	programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages => ["LISP", "C"]}
