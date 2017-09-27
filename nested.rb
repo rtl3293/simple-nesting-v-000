f9243327b6be9b4370d4f302363329159de59d4c
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 22:05:05 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -108,7 +108,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 alans_new_info.to_sym
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+
 
 end
 
